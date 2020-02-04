e24e0b0696b774e15adcaf04fbfae908cf75e48e
Otto K <ottokeres@gmail.com>
2020-02-03 21:47:21 -0500

    added the http client/url and some strucutre in the components

---
+++ src/app/imovie.ts
@@ -0,0 +1,5 @@
+export interface IMovie {
+    rating : number;
+    genre : string;
+    length : number;
+}
--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { IMovie } from '../imovie';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.css']
 })
-export class MovieDetailComponent implements OnInit {
+export class MovieDetailComponent implements IMovie {
+
+  rating : number;
+  genre : string;
+  length : number;
+
+  movieList : IMovie[] = [];
 
   constructor() { }
 
