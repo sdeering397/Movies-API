ad5baa137f03bf98bd84b40176d2212d65180cbc
Spencer Deering <sdeering397@gmail.com>
2020-02-06 18:40:39 -0500

    Added functions for specific searches

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -13,4 +13,24 @@ export class ApiService {
   getMovieList(){
     return this.http.get(this._url);
   }
+
+  _url1='https://api.themoviedb.org/3/discover/movie?api_key=b9e5c9c00c0d2cb749516b3e2ecbfcc1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
+
+  getMovieListByDate(){
+    return this.http.get(this._url1);
+  }
+
+  _url2='https://api.themoviedb.org/3/genre/movie/list?api_key=b9e5c9c00c0d2cb749516b3e2ecbfcc1';
+
+  getListOfGenres(){
+    return this.http.get(this._url2);
+  }
+
+// //baseUrl = this.id
+
+//   //_url3=`https://api.themoviedb.org/3/movie/${this.baseUrl}?api_key=b9e5c9c00c0d2cb749516b3e2ecbfcc1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+
+//   getMovieById(){
+//     return this.http.get(this._url3);
+//   }
 }
